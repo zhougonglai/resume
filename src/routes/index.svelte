fbd6d985f2921366715e78f72c69cc3333eb6922
ZGL-MATEBOOK\周公来 <zhougonglai@nn.com>
2022-03-08 00:39:41 +0800

    update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,9 +16,12 @@
 		<div class="card-content px-4 py-8">
 			<h3 class="card-label">个人信息</h3>
 			<ul class="card-list">
-				<li>姓名: 股哥</li>
-				<li>性别: 男</li>
-				<li>出生: 1992</li>
+				<li>股哥/🚹/1992</li>
+				<li>本科/萌鹿大学计算机系</li>
+				<li>工作年限：2015年~至今</li>
+				<li>期望职位：Web前端高级程序员</li>
+				<li>期望薪资：税前月薪15k~20k，特别喜欢的公司可例外</li>
+				<li>期望城市：武汉</li>
 			</ul>
 			<div class="card-action">
 				<a
@@ -41,6 +44,46 @@
 					<span class="content">微信二维码</span>
 				</button>
 			</div>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="card-content px-4 py-8">
+			<h3 class="card-label">工作经历</h3>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="card-content px-4 py-8">
+			<h3 class="card-label">项目经历</h3>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="card-content px-4 py-8">
+			<h3 class="card-label">技能清单</h3>
+			<ul class="card-list">
+				<li>Web开发：PHP/Hack/Node</li>
+				<li>Web框架：ThinkPHP/Yaf/Yii/Lavaral/LazyPHP</li>
+				<li>前端框架：Bootstrap/AngularJS/EmberJS/HTML5/Cocos2dJS/ionic</li>
+				<li>前端工具：Bower/Gulp/SaSS/LeSS/PhoneGap</li>
+				<li>数据库相关：MySQL/PgSQL/PDO/SQLite</li>
+				<li>版本管理、文档和自动化部署工具：Svn/Git/PHPDoc/Phing/Composer</li>
+				<li>单元测试：PHPUnit/SimpleTest/Qunit</li>
+				<li>云和开放平台：SAE/BAE/AWS/微博开放平台/微信应用开发</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="card">
+		<div class="card-content px-4 py-8">
+			<h3 class="card-label">业余技能</h3>
+			<ul class="card-list">
+				<li>Python</li>
+				<li>Tensorflow</li>
+				<li>Web3</li>
+				<li>Solidity</li>
+			</ul>
 		</div>
 	</div>
 </section>
